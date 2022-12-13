78ac48fbf8f88361f6738ba9b8fc213e2365761b
Joann <Joann@THEJAYPROJECT>
2022-12-13 13:56:16 +0100

    Add project files.

---
+++ tenses.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+void presentSimple() {
+	std::cout << "When using He/She/It" << std::endl << "Add -s to the first form of the verb." << std::endl;
+	std::cout << "When using We/They/You/I" << std::endl << "Use the first form of the verb." << std::endl;
+}
+void pastSimple() {
+	std::cout << "Use the second form of the verb.";
+}
+void futureSimple() {
+	std::cout << "Add 'will' before the first form of the verb.";
+}
+void presentContinuous() {
+	std::cout << "When using I" << std::endl << "'am' + add -ing to the verb." << std::endl;
+	std::cout << "When using He/She/It" << std::endl << "'is' + add -ing to the verb." << std::endl;
+	std::cout << "When using We/They/You" << std::endl << "'are' + add -ing to the verb." << std::endl;
+}
+void pastContinuous() {
+	std::cout << "When using He/She/It/I" << std::endl << "'was' + add -ing to the verb." << std::endl;
+	std::cout << "When using We/They/You" << std::endl << "'were' + add -ing to the verb." << std::endl;
+}
+void futureContinuous() {
+	std::cout << "'will be' + add -ing to the verb." << std::endl;
+}
+void presentPerfectContinuous() {
+	std::cout << "When using He/She/It" << std::endl << "'has been' + add -ing to the verb." << std::endl;
+	std::cout << "When using We/They/You/I" << std::endl << "'have been' + add -ing to the verb." << std::endl;
+}
+void pastPerfectContinuous() {
+	std::cout << "'had been' + add -ing to the verb." << std::endl;
+}
+void futurePerfectContinuous() {
+	std::cout << "'will have been' + add -ing to the verb." << std::endl;
+}
+void presentPerfectSimple() {
+	std::cout << "When using He/She/It" << std::endl << "'has' + use the third form of the verb." << std::endl;
+	std::cout << "When using We/They/You/I" << std::endl << "'have' + use the third form of the verb." << std::endl;
+}
+void pastPerfectSimple() {
+	std::cout << "'had' + use the third form of the verb." << std::endl;
+}
+void futurePerfectSimple() {
+	std::cout << "'will have' + use the third form of the verb." << std::endl;
+}
